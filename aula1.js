a3994e504e30f0733b0cbe3b5251146cdeebf855
Murilo <murilo201107@gmail.com>
2024-04-02 21:37:45 -0300

    msg aula 1

---
+++ aula1.js
@@ -0,0 +1,47 @@
+/*
+
+programa de computador
+
+E .> P .> S
+  
+      
+    
+     javascript NÃO  é igual o JAVA!!!
+    
+     C, c++ , c# , jva , php , js
+     python, ruby, lisp,visual basic  
+    
+     js é fracamente tipado
+     js é interpretado
+     tipagem dinâmica
+
+*/
+      
+   
+var nome = "Murilo"; // texto -> string
+var idade = 23; // numero ->  number -> int 
+var peso = 54.9; // number -> float
+var acordado = true; // boolean -> bool
+
+
+nome = "luluzinha";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
